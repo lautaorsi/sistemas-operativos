cd93f72474aabeddf62e2988b9f4e9f89b5cd1ad
Lautaro <lautaorsi@gmail.com>
2025-09-02 10:45:50 -0300

    Added ex. 1 to 9

---
+++ guias/resoluciones/practica1.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+int ej5(){
+    
+    int pid_hijo = fork();              //Forkeamos el proceso, fork() retorna el PID del hijo para la ejecucion del padre y cero para el hijo   
+    if(!pid_hijo){
+        printf("Soy Homero! \n");       //Unicamente homero ejecuta esta parte, a esta altura estan vivos Abraham y Homero
+
+        int pid_Bart = fork();              //Creamos a Bart
+        if(!pid_Bart){                  //Ejecutado x Bart
+            printf("Soy Bart! \n");
+            exit(1);                    //Muere Bart
+        }
+
+        int pid_Lisa = fork();              //Creamos a Lisa
+        if(!pid_Lisa){                  //Ejecutado x Lisa
+            printf("Soy Lisa! \n");
+            exit(1);                    //Muere Lisa
+        }
+
+        int pid_Maggie = fork();              //Creamos a Maggie
+        if(!pid_Maggie){                  //Ejecutado x Maggie
+            printf("Soy Maggie! \n");
+            exit(1);                    //Muere Maggie
+        }
+
+        waitpid(pid_Bart, NULL, 0);
+        waitpid(pid_Lisa, NULL, 0);
+        waitpid(pid_Maggie, NULL, 0);     //Esperamos a que mueran los 3 
+
+        exit(1);                        //Muere Homero
+    }; 
+    printf("Soy Abraham! \n");          //Ejecutado x Abraham
+    exit(1);                            //Muere Abraham 
+}                                       
+
+void ej6(const char *arg){
+    pid_t pid = fork();
+    execl("/bin/sh", "sh", "-c", arg, (char*) NULL);
+}
+
+void pong(){
+    printf("Pong \n ");
+}
+
+void subrutinaHijo(){
+    signal(SIGTERM,pong);
+    pause();
+}
+
+void ej9(){
+    int keep_going;
+    pid_t pidHijo = fork();
+
+    if(pidHijo == 0) subrutinaHijo();
+    
+    while(1){
+        for(int i = 0; i < 3; i++){
+            printf("Ping \n ");
+            kill(pidHijo, SIGTERM);
+            waitpid(pidHijo, 0, 0);
+            printf(" ");
+        }
+        printf("Want to keep going? [1/0] ");
+        scanf("%d", &keep_going);
+        if(!keep_going) {
+            kill(pidHijo, SIGKILL);
+            exit(1);
+        };
+    }
+
+}
+
+
+int main(){
+    ej9();
+    return 0;
+}
+
---
+++ guias/resoluciones/practica1.md
@@ -0,0 +1,43 @@
+# Practica 1: Procesos y API del SO 
+
+### Ejercicio 1
+
+Debemos guardar una imagen del estado del procesador antes de realizar el cambio, almacenar esa informacion para la tarea que estaba siendo ejecutada (en el **PCB o Process Control Block**) y luego modificar el estado del procesador (esto es: sus registros, pc, sp, etc). <br>
+Ademas, el quantum debera ser actualizado acorde a aquel determinado por la politica de scheduling para el proceso a ejecutar. 
+
+### Ejercicio 2
+Implementar la rutina encargada de realizar el cambio de contexto entre dos procesos (cuyos programas ya han sido cargados en memoria) debido a que el primero ha consumido su quantum. <br>
+
+    void ke_context_switch(PCB* pcb_0, PCB* pcb_1){
+        
+        pcb_0[R0...R15] .=. R0...R15;       ## guardamos los valores de registros en la pcb0
+        R0...R15 .=. pcb_1[R0...R15];       ## actualizamos los registros con la pcb1
+                                            ## (en un muy pseudo-codigo)
+
+        set_current_process(pcb_1.P_ID);    ## actualizamos el process id al del proceso 1
+
+        ke_reset_current_user_time();       ## restarteamos el quantum, asumo que el scheduling usa un sistema que reparte a todos 
+                                            ## la misma cantidad de tiempo o que usa el process id asignado como siguiente a ejecutarse para asignar el quantum correspondiente
+
+        ret();                              ## desapilamos pila y reemplazamos el PC
+    }
+
+
+### Ejercicio 4
+
+TODO
+
+### Ejercicio 5
+[Resuelto en practica1.c](./practica1.c)
+
+### Ejercicio 6
+[Resuelto en practica1.c](./practica1.c)
+
+### Ejercicio 8
+
+No son iguales:
+- Caso hijo entra en el 2do if e incrementa en 3 la variable "dato" local a su contexto.
+- Caso padre entra en el 3er if y no incrementa el dato, manteniendose en 0.
+
+### Ejercicio 9
+[Resuelto en practica1.c](./practica1.c)
